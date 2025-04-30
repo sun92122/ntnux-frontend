578815b9e4e68d9888f2bfa8d4a34980ce266035
magical <sun920122@gmail.com>
2025-04-30 21:56:01 +0800

    ✨ 簡易頁面框架及深色模式支持

--- app.vue
+++ app.vue
@@ -1,5 +1,94 @@
 <template>
   <div id="app">
+    <Menubar :model="items" :sticky="true">
+      <template #end>
+        <div class="menubar-end">
+          <ToggleSwitch
+            v-model="darkMode"
+            class="me-2"
+            @change="toggleDarkMode"
+            aria-label="Toggle Dark Mode"
+            :dt="toggleSwitchDt"
+          >
+            <template #handle>
+              <i
+                :class="{ 'pi pi-sun': !darkMode, 'pi pi-moon': darkMode }"
+              ></i>
+            </template>
+          </ToggleSwitch>
+          <Button
+            icon="pi pi-github"
+            class="p-button-rounded p-button-secondary"
+            :url="'https://github.com/sun92122/NTNUx'"
+            target="_blank"
+            aria-label="Github"
+          ></Button>
+        </div>
+      </template>
+    </Menubar>
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+import Menubar from "primevue/menubar";
+import Button from "primevue/button";
+import ToggleSwitch from "primevue/toggleswitch";
+
+const items = ref([
+  {
+    label: "首頁",
+    icon: "pi pi-fw pi-home",
+    route: "/",
+  },
+  {
+    label: "課表",
+    icon: "pi pi-fw pi-calendar",
+    route: "/schedule",
+  },
+  {
+    label: "關於",
+    icon: "pi pi-fw pi-info-circle",
+    route: "/about",
+  },
+]);
+
+const darkMode = useState("darkMode", () => false);
+const toggleDarkMode = () => {
+  if (darkMode.value) {
+    document.documentElement.classList.add("dark-mode-toggle");
+  } else {
+    document.documentElement.classList.remove("dark-mode-toggle");
+  }
+};
+
+const toggleSwitchDt = ref({
+  width: "3.5rem",
+  height: "2rem",
+  handle: {
+    size: "1.5rem",
+    checked: {
+      color: "#FFFFFF",
+      background: "#000000",
+      hover: {
+        color: "#FFFFFF",
+        background: "#000000",
+      },
+    },
+  },
+  checked: {
+    background: "#FFFFFF",
+    hover: {
+      background: "#FFFFFF",
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.menubar-end {
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primeuix/themes/aura";
+
 export default defineNuxtConfig({
   app: {
     baseURL: "/", // 使用自訂網域，保留為根路徑
   },
   nitro: {
     preset: "static",
+  },
+
+  modules: ["@primevue/nuxt-module"],
+
+  css: ["primeicons/primeicons.css"],
+
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          dark: false,
+          darkModeSelector: ".dark-mode-toggle",
+        },
+      },
+    },
+    components: {
+      prefix: "Prime",
+      exclude: ["Editor", "Chart", "Form", "FormField"], // To fix import error from PrimeVue
+    },
   },
 
   devtools: { enabled: true },
